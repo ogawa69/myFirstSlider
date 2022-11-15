e87db3138373790841c81145afc8b073741a94d0
ogawa69 <38399063+ogawa69@users.noreply.github.com>
2022-11-15 13:17:23 +0400

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,23 @@
-const slider = document.querySelector('.swiper-container');
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.querySelector('.swiper-container');
+  const isMobile = window.matchMedia("(max-width: 767.98px)");
+  const isTabletAndDesktop = window.matchMedia("(min-width: 768px)");
 
-let mySwiper = new Swiper(slider, {
-	slidesPerView: 1.2,
-	spaceBetween: 16,
-	loop: true,
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
-});
-
-if (window.innerWidth < 768 && slider.dataset.mobile == 'false') {
-    mySwiper();
-
-    slider.dataset.mobile = 'true';
-  }
-
-if (window.innerWidth >= 768) {
-    slider.dataset.mobile = 'false';
-    if (slider.classList.contains('swiper-container-initialized')) {
-      mySwiper.destroy();
+  const mySwiper = new Swiper(slider, {
+    slidesPerView: 1.2,
+    spaceBetween: 16,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+  
+  if (isMobile.matches) {
+      mySwiper();
     }
-  }
+  
+  if (isTabletAndDesktop.matches) {
+        mySwiper.destroy();
+    }
+});
